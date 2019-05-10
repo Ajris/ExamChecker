2d2d1757ad37d0b54b76c28f64971d4dd3e20e75
Ajris <hubiflower@gmail.com>
2019-05-10 20:03:44 +0200

    small fix

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pdf
 import pdf_to_jpg
+import detect_square
+
 pdf.simple_table_html()
 pdf_to_jpg.convert('simple_table_html.pdf')
+detect_square.find_squares()
--- pdf_to_jpg.py
+++ pdf_to_jpg.py
@@ -2,6 +2,6 @@ from pdf2image import convert_from_path
 
 
 def convert(filename):
-    pages = convert_from_path(filename, 500)
+    pages = convert_from_path(filename, 300)
     for page in pages:
         page.save('out.jpg', 'JPEG')
