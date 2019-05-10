e8134958404c8a5a67ee7c3ac2c1ba620bb50c2a
Maciek <maciej.wyzlinski@gmail.com>
2019-05-10 18:43:47 +0200

    html table

--- pdf.py
+++ pdf.py
@@ -1,23 +1,20 @@
 # simple_table_html.py
  
 from fpdf import FPDF, HTMLMixin
-
-import HTML
-table_data = [
-        ['Last name',   'First name',   'Age'],
-        ['Smith',       'John',         30],
-        ['Carpenter',   'Jack',         47],
-        ['Johnson',     'Paul',         62],
-    ]
-htmlcode = HTML.table(table_data)
-
+ 
 class HTML2PDF(FPDF, HTMLMixin):
     pass
- 
+
 def simple_table_html():
     pdf = HTML2PDF()
  
-    table = htmlcode 
+    table = """<table border="0" align="center" width="50%">
+    <thead><tr><th width="30%">Header 1</th><th width="70%">header 2</th></tr></thead>
+    <tbody>
+    <tr><td>cell 1</td><td>cell 2</td></tr>
+    <tr><td>cell 2</td><td>cell 3</td></tr>
+    </tbody>
+    </table>"""
  
     pdf.add_page()
     pdf.write_html(table)
