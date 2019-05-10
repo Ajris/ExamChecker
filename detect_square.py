b83f3a950dde4cae6e57d3622542eea341a07755
Maciek <maciej.wyzlinski@gmail.com>
2019-05-11 01:02:06 +0200

    Detecting square coordinates

--- detect_square.py
+++ detect_square.py
@@ -13,7 +13,11 @@ def find_squares(read_from, save_to, x, y):
     el = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
     img = cv.erode(img, el, iterations=1)
     squares = []
-    middles = []
+    middles1 = [], []
+    middles2 = [], []
+    middles3 = [], []
+    middles4 = [], []
+    width, height = img.shape 
     for gray in cv.split(img):
         for thrs in range(0, 255, 26):
             if thrs == 0:
@@ -29,13 +33,28 @@ def find_squares(read_from, save_to, x, y):
                     cnt = cnt.reshape(-1, 2)
                     max_cos = np.max([angle_cos(cnt[i], cnt[(i + 1) % 4], cnt[(i + 2) % 4]) for i in range(4)])
                     if max_cos < 0.1:
-                        m = np.arange(2).reshape(1, 2)
-                        m[0] = [(cnt[0][0] + cnt[2][0]) / 2, (cnt[0][1] + cnt[2][1]) / 2]
-                        middles.append(m)
+                        x_mid, y_mid = (cnt[0][0] + cnt[2][0]) / 2, (cnt[0][1] + cnt[2][1]) / 2
+                        if x_mid < width/2 and y_mid < height/2: 
+                            middles1[0].append(x_mid)
+                            middles1[1].append(y_mid)
+                        elif x_mid > width/2 and y_mid < height/2: 
+                            middles2[0].append(x_mid)
+                            middles2[1].append(y_mid)
+                        elif x_mid < width/2 and y_mid > height/2: 
+                            middles3[0].append(x_mid)
+                            middles3[1].append(y_mid)
+                        elif x_mid > width/2 and y_mid > height/2: 
+                            middles4[0].append(x_mid)
+                            middles4[1].append(y_mid)
+
                         squares.append(cnt)
     nowe = cv.imread(read_from, cv.IMREAD_ANYCOLOR)
+    top_left = sum(middles1[0])/len(middles1[0]) , sum(middles1[1])/len(middles1[1])
+    top_right = sum(middles2[0])/len(middles2[0]) , sum(middles2[1])/len(middles2[1])
+
+    print(top_left, top_right)
     cv.drawContours(nowe, squares, -1, (0, 255, 0), 3)
-    cv.drawContours(nowe, middles, -2, (255, 255, 0), 3)
+    #  cv.drawContours(nowe, middles, -2, (255, 255, 0), 3)
 
     # res = []
     # for a in middles:
@@ -46,8 +65,10 @@ def find_squares(read_from, save_to, x, y):
     # print(res)
     #88 88 737 88
     #
+    scale = (top_right[0] - top_left[0])/165.0
     for i in x:
         for j in y:
-            cv.circle(nowe, (int(i*650/180) + middles[1][0][0]+35, int(j*650/180) + middles[61][0][1]+15), 4, (0, 0, 255), 3)
+            pos = int(i*scale + np.floor(top_left[0])), int(j*scale +  np.floor(top_left[1]))
+            cv.circle(nowe, pos, 4, (0, 0, 255), 3)
 
     cv.imwrite(save_to, nowe)
