0216aa15025d2d5d7cb0f9fc1378b87c27e3fbfd
Maciek <maciej.wyzlinski@gmail.com>
2019-05-10 23:17:52 +0200

    square coordinates

--- table_generator.py
+++ table_generator.py
@@ -6,10 +6,11 @@ class AnswerSheetGenerator(FPDF):
     box_width = 6
     box_h_space = 10
     box_v_space = 10
-    answers = dict() 
-    answers_pos = dict()
+    answers_y_pos = []
+    answers_x_pos = []
     width = None
     height = None
+
     def __init__(self, width, height, random_answer = False):
         FPDF.__init__(self, unit = 'mm', format='A4')
         self.add_page()
@@ -17,6 +18,7 @@ class AnswerSheetGenerator(FPDF):
         self.width = width
         self.height = height
         self.generate_table(width, height, random_answer)
+        self.generate_reference_rectangles()
 
     def get_x(self, width):
         return (210 - self.box_h_space*(width + 2))/2
@@ -33,31 +35,39 @@ class AnswerSheetGenerator(FPDF):
     def generate_table(self, width, height, random_answer = False):
         self.set_y(20)
         for i in range(width):
-            self.set_x(self.get_x(width) + self.box_h_space*(i + 1.5) - self.box_width/2)
+            x_pos = self.get_x(width) + self.box_h_space*(i + 1.5) - self.box_width/2
+            self.set_x(x_pos)
             self.cell(self.box_width, self.box_width, chr(i + ord('A')), border = 0)
+            self.answers_x_pos.append(x_pos - 22.5)
         for i in range(height):
-            self.set_y(i*self.box_v_space + 30)
+            y_pos = i*self.box_v_space + 30
+            self.set_y(y_pos)
             self.set_x(self.get_x(width))
             self.cell(self.box_width, self.box_width, str(i + 1))
             self.add_row(width, random_answer)
-    
-    #  def get_answer_positions(self):
-        
-    #  def get_answer_positions(self
-    #  def set_answer(self, question, answer):
-        #  self.answers.update(question : answer)
+            self.answers_y_pos.append(y_pos - 22.5)
 
-    def get_answer(self, question, answer):
-        return self.answers.get(question)
+    def generate_reference_rectangles(self):
+        self.rect(15, 15, 15, 15)
+        self.rect(210 - 30, 15, 15, 15)
+        self.rect(210 - 30, 297 - 30, 15, 15)
+        self.rect(15, 297 - 30, 15, 15)
+    
+
+        
+
+    #  def get_answer(self, question, answer):
+        #  return self.answers.get(question)
 
 
 def generate_table(name, width, height):
     pdf = AnswerSheetGenerator(width, height) 
     pdf.output(name)
-
-def random_answer_table(name, width, height):
-    pdf = AnswerSheetGenerator(width, height, True) 
-    pdf.output(name)
+    print(pdf.answers_x_pos)
+    print(pdf.answers_y_pos)
+#  def random_answer_table(name, width, height):
+    #  pdf = AnswerSheetGenerator(width, height, True) 
+    #  pdf.output(name)
 
 generate_table('table.pdf', 4, 25)
-random_answer_table('random.pdf', 4, 25)
+#  random_answer_table('random.pdf', 4, 25)
