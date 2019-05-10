2e5c66d310d38f67fff9f2fdf39be3f6dde7907c
Ajris <hubiflower@gmail.com>
2019-05-10 20:59:01 +0200

    fixed changing

--- detect_square.py
+++ detect_square.py
@@ -1,10 +1,4 @@
-import numpy as np
 import cv2 as cv
-
-
-def angle_cos(p0, p1, p2):
-    d1, d2 = (p0 - p1).astype('float'), (p2 - p1).astype('float')
-    return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1, d1) * np.dot(d2, d2)))
 
 
 def find_squares(read_from, save_to):
@@ -14,13 +8,12 @@ def find_squares(read_from, save_to):
     el = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
     img = cv.dilate(img, el, iterations=1)
     squares = []
-    for thrs in range(0, 255, 26):
-        contours, _hierarchy = cv.findContours(img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-        for cnt in contours:
-            cnt_len = cv.arcLength(cnt, True)
-            cnt = cv.approxPolyDP(cnt, 0.02 * cnt_len, True)
-            if len(cnt) == 4 and 1 < cv.contourArea(cnt) < 200000 and cv.isContourConvex(cnt):
-                squares.append(cnt)
+    contours, _hierarchy = cv.findContours(img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        cnt_len = cv.arcLength(cnt, True)
+        cnt = cv.approxPolyDP(cnt, 0.02 * cnt_len, True)
+        if len(cnt) == 4 and 1 < cv.contourArea(cnt) < 200000 and cv.isContourConvex(cnt):
+            squares.append(cnt)
     nowe = cv.imread(read_from, cv.IMREAD_ANYCOLOR)
-    cv.drawContours(nowe, squares, -1, (0, 255, 0), 3)
+    cv.drawContours(nowe, squares, -1, (0, 255, 255), 3)
     cv.imwrite(save_to, nowe)
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pdf_to_jpg
 import detect_square
 
-
 PDF_NAME = 'data/table.pdf'
 CONVERTED_PDF = 'data/convertedPDF.jpg'
 OUTPUT = 'data/out.jpg'
 
-pdf_to_jpg.convert(PDF_NAME, CONVERTED_PDF)
+# pdf_to_jpg.convert(PDF_NAME, CONVERTED_PDF)
 detect_square.find_squares(CONVERTED_PDF, OUTPUT)
