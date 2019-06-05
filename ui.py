89723ef1bd014bca03aecd4ca4028d7824451bfe
Ajris <hubiflower@gmail.com>
2019-06-05 23:19:39 +0200

    removed macaron

--- ui.py
+++ ui.py
@@ -6,20 +6,22 @@ import detect_square
 from PIL import Image
 
 window = Tk()
- 
+
 window.title("Welcome to LikeGeeks app")
- 
+
 window.geometry('500x900')
 
 questions_number = IntVar()
 questions_number.set(25)
- 
+
 answers_number = IntVar()
 answers_number.set(4)
 
 questions = []
 question_values = []
 question_nums = []
+
+
 def add_radiobuttons(question, answers):
     global questions
     global question_nums
@@ -32,12 +34,13 @@ def add_radiobuttons(question, answers):
         v = IntVar()
         question_values.append(v)
         lb = Label(window, text=str(i))
-        lb.grid(column = 0, row = i + 3)
+        lb.grid(column=0, row=i + 3)
         question_nums.append(lb)
         for j in range(answers):
             rad = Radiobutton(window, text=chr(j + ord('A')), value=j, variable=v)
-            rad.grid(column = j + 1, row = i + 3)
+            rad.grid(column=j + 1, row=i + 3)
             questions[i].append(rad)
+
 
 def update_questions():
     global questions
@@ -49,8 +52,10 @@ def update_questions():
             j.grid_forget()
     add_radiobuttons(questions_number.get(), answers_number.get())
 
+
 def generate_pdf():
     generate_table("output.pdf", answers_number.get(), questions_number.get())
+
 
 def check_answers():
     RANDOM_PDF = 'data/pdf/chuj.pdf'
@@ -63,7 +68,7 @@ def check_answers():
     if len(answers) == len(question_values):
         correct = 0
         for i in range(len(answers)):
-            print(str(answers[i]) + " " +  str(question_values[i].get()))
+            print(str(answers[i]) + " " + str(question_values[i].get()))
             if answers[i] == question_values[i].get():
                 correct += 1
         print(correct)
@@ -72,8 +77,8 @@ def check_answers():
 
     Image.open(RANDOM_OUTPUT).show()
 
-spin = Spinbox(window, from_=0, to=100, width=5)
 
+spin = Spinbox(window, from_=0, to=100, width=5)
 
 lbl = Label(window, text="Number of questions: ")
 lbl.grid(column=0, row=0)
@@ -84,18 +89,20 @@ spin.grid(column=1, row=0)
 lbl = Label(window, text="Number of answers: ")
 lbl.grid(column=0, row=1)
 
-spin = Spinbox(window, from_=2, to=6, width=5, textvariable=answers_number, command = update_questions)
+spin = Spinbox(window, from_=2, to=6, width=5, textvariable=answers_number, command=update_questions)
 spin.grid(column=1, row=1)
 
+
 def clicked():
-   print(answers_number.get())
-   print(questions_number.get())
+    print(answers_number.get())
+    print(questions_number.get())
+
 
 add_radiobuttons(questions_number.get(), answers_number.get())
 
 btn = Button(window, text="Generate pdf", command=generate_pdf)
-btn.grid(column = 2, row = 0)
+btn.grid(column=2, row=0)
 btn = Button(window, text="Check answers", command=check_answers)
-btn.grid(column = 2, row = 1)
+btn.grid(column=2, row=1)
 
 window.mainloop()
