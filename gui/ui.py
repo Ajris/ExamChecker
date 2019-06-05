40b58581171cb552e31f158d45c842e7115f2f52
Ajris <hubiflower@gmail.com>
2019-06-06 00:50:57 +0200

    chaning ui

--- gui/ui.py
+++ gui/ui.py
@@ -5,21 +5,43 @@ from gui import pdf_to_jpg
 from table.checker import detect_square
 from PIL import Image
 
-window = Tk()
-
-window.title("Welcome to LikeGeeks app")
-
-window.geometry('500x900')
-
-questions_number = IntVar()
-questions_number.set(25)
-
-answers_number = IntVar()
-answers_number.set(4)
-
 questions = []
 question_values = []
 question_nums = []
+
+
+def clear_without_menu():
+    for child in window.winfo_children():
+        if child.winfo_class() != 'Menu':
+            child.destroy()
+
+
+def generate_function():
+    clear_without_menu()
+    lbl = Label(window, text="Number of questions: ")
+    lbl.grid(column=0, row=0)
+
+    spin = Spinbox(window, from_=0, to=25, width=5, textvariable=questions_number, command=update_questions)
+    spin.grid(column=1, row=0)
+
+    lbl = Label(window, text="Number of answers: ")
+    lbl.grid(column=0, row=1)
+
+    spin = Spinbox(window, from_=2, to=6, width=5, textvariable=answers_number, command=update_questions)
+    spin.grid(column=1, row=1)
+    add_radiobuttons(questions_number.get(), answers_number.get())
+
+    btn = Button(window, text="Generate pdf", command=generate_pdf)
+    btn.grid(column=4, row=100)
+
+
+def check_function():
+    clear_without_menu()
+    Label(window, text="Username").grid(row=10)  # this is placed in 0 0
+    Entry(window).grid(row=0, column=1)
+
+    btn = Button(window, text="Check answers", command=check_answers)
+    btn.grid(column=2, row=1)
 
 
 def add_radiobuttons(question, answers):
@@ -78,31 +100,25 @@ def check_answers():
     Image.open(RANDOM_OUTPUT).show()
 
 
-spin = Spinbox(window, from_=0, to=100, width=5)
+window = Tk()
+window.title("Exam Checker")
+window.geometry('500x900')
 
-lbl = Label(window, text="Number of questions: ")
-lbl.grid(column=0, row=0)
+questions_number = IntVar()
+questions_number.set(25)
 
-spin = Spinbox(window, from_=0, to=25, width=5, textvariable=questions_number, command=update_questions)
-spin.grid(column=1, row=0)
+answers_number = IntVar()
+answers_number.set(4)
 
-lbl = Label(window, text="Number of answers: ")
-lbl.grid(column=0, row=1)
+root_menu = Menu(window)
+window.config(menu=root_menu)
 
-spin = Spinbox(window, from_=2, to=6, width=5, textvariable=answers_number, command=update_questions)
-spin.grid(column=1, row=1)
+generate_menu = Menu(root_menu)
+root_menu.add_cascade(label="Generate", menu=generate_menu)
+generate_menu.add_command(label="Generate PDF", command=generate_function)
 
-
-def clicked():
-    print(answers_number.get())
-    print(questions_number.get())
-
-
-add_radiobuttons(questions_number.get(), answers_number.get())
-
-btn = Button(window, text="Generate pdf", command=generate_pdf)
-btn.grid(column=2, row=0)
-btn = Button(window, text="Check answers", command=check_answers)
-btn.grid(column=2, row=1)
+check_menu = Menu(root_menu)
+root_menu.add_cascade(label="Check", menu=check_menu)
+check_menu.add_command(label="Check PDF", command=check_function)
 
 window.mainloop()
